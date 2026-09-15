8b1ffd0ae1b0e6fddc531099287ab3340866be70
solomon-israel793 <solomonisrael793@gmail.com>
2026-09-15 16:28:44 +0100

    Update courseRoutes.js

--- courseRoutes.js
+++ courseRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const { protect, authorize } = require('.auth');
-const { getCourses, createCourse, updateCourse, deleteCourse } = require('.courseController');
+const { protect, authorize } = require('./auth');
+const { getCourses, createCourse, updateCourse, deleteCourse } = require('./courseController');
 
 router.use(protect);
 
